7cd0d0fc0b882256f468b22ba43be15bd7fb7b06
Areum Jo <areumjo1@gmail.com>
2019-06-19 15:41:15 -0400

    Imported Articles.less and fixed the function on the method

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,7 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-  console.log('menu button clicked?')
+  // console.log('menu button clicked?')
 
   menu.classList.toggle('menu--open');
 
@@ -12,8 +12,8 @@ const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 
-console.log(menu);
-console.log(menuButton);
+// console.log(menu);
+// console.log(menuButton);
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', () => toggleMenu());
