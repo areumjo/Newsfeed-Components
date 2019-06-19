3ea7df49d6ea6b7e667c0e9b31b27c679a102696
Areum Jo <areumjo1@gmail.com>
2019-06-19 15:27:26 -0400

    Imported Menu.less and fixed toggleMenu function

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,19 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  console.log('menu button clicked?')
+
+  menu.classList.toggle('menu--open');
+
 }
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button');
+
+console.log(menu);
+console.log(menuButton);
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', () => toggleMenu());
